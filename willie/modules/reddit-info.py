50ad3b01b4861fb625b75fd4a96ab3b3f751a660
miggyb <miggyb@berrytube.tv>
2013-05-30 12:30:59 +0000

    Rolled back reddit-info module to fix incompatibilities

--- willie/modules/reddit-info.py
+++ willie/modules/reddit-info.py
@@ -19,21 +19,8 @@ def setup(willie):
         exclude = willie.memory['url_exclude']
         exclude.append(regex)
         willie.memory['url_exclude'] = exclude
-domain = r'https?://(?:www\.|np\.)?reddit\.com'
-post_url = '(%s/r/.*?/comments/[\w-]+)' % domain
-user_url = '%s/u(ser)?/([\w-]+)' % domain
 
-
-def setup(willie):
-    post_regex = re.compile(post_url)
-    user_regex = re.compile(user_url)
-    if not willie.memory.contains('url_callbacks'):
-        willie.memory['url_callbacks'] = {}
-    willie.memory['url_callbacks'][post_regex] = rpost_info
-    willie.memory['url_callbacks'][user_regex] = redditor_info
-
-
-def rpost_info(willie, trigger, match=None):
+def rpost_info(willie, trigger):
     r = praw.Reddit(user_agent='phenny / willie IRC bot - see dft.ba/-williesource for more')
     s = r.get_submission(submission_id=trigger.group(1))
 
@@ -47,14 +34,6 @@ def rpost_info(willie, trigger, match=None):
         author = s.author.name
     else: author = '14[deleted]'
 
-    match = match or trigger
-    s = r.get_submission(url=match.group(1))
-
-    message = '[REDDIT] ' + s.title
-    if s.is_self:
-        message = message + ' (self.' + s.subreddit.display_name + ')'
-    else:
-        message = message + ' (' + s.url + ')' + ' to r/' + s.subreddit.display_name
     if s.over_18:
         title = '05[NSFW] ' + title + ' 05[NSFW]'
         #TODO implement per-channel settings db, and make this able to kick
@@ -99,39 +78,33 @@ def rpost_info(willie, trigger, match=None):
         time, units, author, s.subreddit.display_name))  
 
     willie.say(message)
+
 rpost_info.rule = '.*(?:(?:https?://)?(?:www\.)?(?:\w\w(?:\-\w\w)\.)?)?(?:(?:reddit\.com/(?:(?:r/\w+/comments)|(?:tb)))|redd\.it)/(\w+)'
 
-def redditor_info(willie, trigger, match=None):
+def redditor_info(willie, trigger):
     """Show information about the given Redditor"""
-    commanded = re.match(willie.config.prefix + 'redditor', trigger)
+    commanded = re.match(willie.config.prefix+'.*', trigger)
     r = praw.Reddit(user_agent='phenny / willie IRC bot - see dft.ba/-williesource for more')
-    match = match or trigger
     try:
-        u = r.get_redditor(match.group(2))
+        u = r.get_redditor(trigger.group(2))
     except:
         if commanded:
             willie.say('No such Redditor.')
-            return willie.NOLIMIT
-        else:
-            return
+        return
         #Fail silently if it wasn't an explicit command.
-
-    message = '[REDDITOR] ' + u.name
-    if commanded:
-        message = message + ' | http://reddit.com/u/' + u.name
-    if u.is_gold:
-        message = message + ' | 08Gold'
-    if u.is_mod:
-        message = message + ' | 05Mod'
-    message = message + ' | Link: ' + str(u.link_karma) + ' | Comment: ' + str(u.comment_karma)
-
+    
+    message = '[REDDITOR] '+u.name
+    if commanded: message = message + ' | http://reddit.com/u/'+u.name
+    if u.is_gold: message = message + ' | 08Gold'
+    if u.is_mod: message = message + ' | 05Mod'
+    message = message + ' | Link: '+str(u.link_karma)+ ' | Comment: '+str(u.comment_karma)
+    
     #TODO detect cake day with u.created
     willie.say(message)
 #If you change this, you'll have to change some things above.
 redditor_info.commands = ['redditor']
 
-
 def auto_redditor_info(willie, trigger):
     redditor_info(willie, trigger)
 #If you change the groups here, you'll have to change some things above.
-auto_redditor_info.rule = '.*%s.*' % user_url
+auto_redditor_info.rule = '.*http(?:s)?://(?:www\.)?reddit\.com/u(ser)?/([\w-]+).*'
